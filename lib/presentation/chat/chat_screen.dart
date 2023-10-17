024ef04b1613fc310fd7c1952b806f3871d3c917
LeonParedes <a19490947@itmexicali.edu.mx>
2023-10-16 22:32:04 -0700

    codigo16/10
    base para un chat

---
+++ lib/presentation/chat/chat_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+
+class ChatScreen extends StatelessWidget{
+  
+
+  
+  Widget build(BuildContext context) {
+   return Scaffold(
+    appBar:  AppBar(
+      leading:  const Padding(
+        padding: EdgeInsets.all(4.0),
+        child: CircleAvatar(
+          backgroundImage:  NetworkImage('https://image.enjoymovie.net/0lCFKCq3DeQl8BALK7mR59-Itf4=/256x256/smart/core/p/3OWy6o6vwM.jpg'),
+        ),
+      ),      
+      title: const Text('A nya'),
+      centerTitle: false,
+   
+    ),
+    body: _ChatView(),    
+    );
+  }
+
+}
+class _ChatView extends StatelessWidget{
+
+Widget build(BuildContext context) {
+   return SafeArea(
+    child: Padding(padding: const EdgeInsets.all(4.0),
+    child: Column(
+      children: [
+        Expanded(child: Container(color: Colors.pink[900])),
+      const Text("Holis"),
+      const Text("Mundo")
+
+    ]),)
+    
+    );
+
+
+  }
+  }
