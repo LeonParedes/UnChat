09b88e620589a97018f3bac944a2ae079148e679
LeonParedes <a19490947@itmexicali.edu.mx>
2023-10-16 22:52:37 -0700

    actualizacion

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,17 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget { 
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+     
+        primarySwatch: Colors.teal
       ),
       home: ChatScreen(),
     );
--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -7,11 +7,11 @@ class ChatScreen extends StatelessWidget{
   
   Widget build(BuildContext context) {
    return Scaffold(
-    appBar:  AppBar(
+    appBar:  AppBar(  
       leading:  const Padding(
         padding: EdgeInsets.all(4.0),
         child: CircleAvatar(
-          backgroundImage:  NetworkImage('https://image.enjoymovie.net/0lCFKCq3DeQl8BALK7mR59-Itf4=/256x256/smart/core/p/3OWy6o6vwM.jpg'),
+          backgroundImage: NetworkImage('https://image.enjoymovie.net/0lCFKCq3DeQl8BALK7mR59-Itf4=/256x256/smart/core/p/3OWy6o6vwM.jpg'),
         ),
       ),      
       title: const Text('A nya'),
@@ -30,7 +30,7 @@ Widget build(BuildContext context) {
     child: Padding(padding: const EdgeInsets.all(4.0),
     child: Column(
       children: [
-        Expanded(child: Container(color: Colors.pink[900])),
+        Expanded(child: Container(color: Colors.pink[200])),
       const Text("Holis"),
       const Text("Mundo")
 
